10d3bccd19bf1cf849cc0d6c6d18931fb9add02a
Ben Black <weepingwillowben@gmail.com>
2017-09-23 22:35:08 -0700

    new test running, not fully built

--- test-new-in.cpp
+++ test-new-in.cpp
@@ -2,21 +2,38 @@
 #include <fstream>
 #include <vector>
 #include <string>
-#include <cinttypes>
 
 using namespace std;
 
-struct MemAccess{
 
+struct MemAccess{
+    size_t instruction;
+    size_t memory;
 };
 
-vector<
+vector<MemAccess> all_accesses;
+
+vector<MemAccess> load_accesses(char * filename){
+    vector<MemAccess> res;
+    
+    ifstream myfile(filename);
+    
+    if (myfile.is_open()) {
+        size_t ins,mem;
+        while ((myfile >> ins) && (myfile >> mem)) {
+            MemAccess acc = {ins,mem};
+            res.push_back(acc);
+        }
+    }
+    return res;
+}
 
 int main(int argc, char ** argv){
-    if(argc != 1){
-        cout << "needs 1 argument\n";
+    if(argc != 2){
+        cout << "needs exactly 1 argument, a input filename\n";
         exit(1);
     }
-    char * filename = argc[0];
-    ifstream myfile
+    char * filename = argv[1];
+    vector<MemAccess> accesses = load_accesses(filename);
+    
 }
