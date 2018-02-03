21e25a7f3cede7c568698ad7995af9bbcb72863a
ben black <weepingwillowben@gmail.com>
2018-02-03 11:26:05 -0800

    fixed types file

--- src/Types.h
+++ src/Types.h
@@ -5,19 +5,26 @@ typedef long long int64_t;
 typedef unsigned long long uint64_t;
 #else
 //#ifdef __PIN__
-typedef unsigned long long uint64_t;
+//typedef unsigned long long uint64_t;
 //#endif
+#include <stdint.h>
 #include <cstdlib>
 #endif
 
 inline int64_t iabs(int64_t num){
     return num >= 0 ? num : -num;
 }
+#ifndef __PIN__
 inline int64_t my_clock(){
     timespec spc;
     clock_gettime(CLOCK_MONOTONIC,&spc);
     return spc.tv_nsec + 1000000000LL*spc.tv_sec;
 }
+#else
+inline int64_t my_clock(){
+    return 0;
+}
+#endif
 
 //#include <unordered_map>
 #define unordered_map map
