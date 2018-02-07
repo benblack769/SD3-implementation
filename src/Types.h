7c5b5a5fbb31cdafc4942ee193ba4c8a1df42de7
ben black <weepingwillowben@gmail.com>
2018-02-06 22:20:13 -0800

    fixed types header

--- src/Types.h
+++ src/Types.h
@@ -1,15 +1,7 @@
 #pragma once
 #include <ctime>
-#ifdef _WIN32
-typedef long long int64_t;
-typedef unsigned long long uint64_t;
-#else
-//#ifdef __PIN__
-//typedef unsigned long long uint64_t;
-//#endif
 #include <stdint.h>
 #include <cstdlib>
-#endif
 
 inline int64_t iabs(int64_t num){
     return num >= 0 ? num : -num;
